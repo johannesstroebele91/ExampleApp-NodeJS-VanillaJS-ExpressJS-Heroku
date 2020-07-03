4243b959a5186d8c1f08b801cfd5daa04c2fcec6
JohannesStroebele <stroebele.j@gmail.com>
2020-07-03 16:54:45 +0200

    Setup basic routing, example pages, scss, and adjusted app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+
+// Routing
+app.use(express.static(__dirname +'/public/pages',{index: false,extensions:['html']}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+module.exports = app;
