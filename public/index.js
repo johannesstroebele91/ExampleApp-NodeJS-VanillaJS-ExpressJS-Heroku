29a28f29f35f756a69d23b61b22b5ef1cf40d27b
JohannesStroebele <stroebele.j@gmail.com>
2020-07-26 15:17:21 +0200

    Added usage of packages from node_modules, added reusable nav bar and footer, and switched to css

---
+++ public/index.js
@@ -0,0 +1,10 @@
+// Navigation on multiple pages
+$(function () {
+    $("#nav-placeholder").load("components/nav.html");
+});
+
+// Footer on multiple pages
+$(function () {
+    $("#footer-placeholder").load("components/footer.html");
+});
+
